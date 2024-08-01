dec1d025173dc5d4a5875d045c1abfb859860d68
Zyy-La <danunurohim@gmail.com>
2024-08-01 18:43:30 +0700

    config minicourse revou

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
 //ini javascript
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+const totalSlides = slides.length;
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.style.transform = `translateX(${(i - index) * 100}%)`;
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % totalSlides;
+    showSlide(currentSlide);
+}
+
+function prevSlide() {
+    currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+    showSlide(currentSlide);
+}
+
+// Initialize
+showSlide(currentSlide);
